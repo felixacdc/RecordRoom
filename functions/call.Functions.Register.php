948bca7523a998b3d93e3453cc951a97e176348d
felixacdc <pfmata01@gmail.com>
2016-02-06 02:44:25 -0600

    [add] DB y try catch

--- functions/call.Functions.Register.php
+++ functions/call.Functions.Register.php
@@ -3,21 +3,29 @@
 require_once '../classes/class.Connection.php';
 require_once '../classes/class.FNRegister.php';
 
-if ( $_POST ) {
+try {
 
-	if ($_POST['name'] != '' or $_POST['lastname'] != '' or $_POST['email'] != '' or $_POST['phone'] != '') {
-		$fnAdd = new Register($_POST['name'], $_POST['lastname'], $_POST['email'], $_POST['phone']);
+	if ( $_POST ) {
 
-		$fnAdd->addRegister();
+		if ($_POST['name'] != '' or $_POST['lastname'] != '' or $_POST['email'] != '' or $_POST['phone'] != '') {
+			$fnAdd = new Register($_POST['name'], $_POST['lastname'], $_POST['email'], $_POST['phone']);
+
+			$fnAdd->addRegister();
+		} else {
+			header("location: ../index.php");
+		}
+
 	} else {
-		header("location: ../index.php");
+		$fnVerify = new Register(0, 0, 0, 0);
+
+		echo $fnVerify->fullRegisters();
 	}
 
-} else {
-	$fnVerify = new Register(0, 0, 0, 0);
-
-	echo $fnVerify->fullRegisters();
+} catch (Exception $e) {
+	header("location: ../index.php");
 }
+
+
 
 
  ?>
