e25787bae4584e31ae5e2215f23c4371e2908dc1
felixacdc <pfmata01@gmail.com>
2016-02-06 23:13:12 -0600

    [modify] conection

--- classes/class.Connection.php
+++ classes/class.Connection.php
@@ -6,9 +6,9 @@ class connectionClass extends PDO
 	// private $db = 'DBMarbella';
 	// private $user = 'root';
 	// private $password = '';
-	private $db = 'goreu_DBMarbella';
-	private $user = 'goreu';
-	private $password = 'RonyGO2012';
+	private $db = 'marbella_DBMarbella';
+	private $user = 'marbella';
+	private $password = 'Guate.16m';
 
 	public function __construct(){
 		parent::__construct("mysql:host=$this->servidor;dbname=$this->db;charset=utf8", "$this->user", $this->password);
